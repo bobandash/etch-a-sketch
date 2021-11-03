0894312e43ba9efc889b5af980f607125b255252
Bruce Hsu <brucehsu1126@gmail.com>
2021-11-02 21:28:52 -0700

    finished the clear grid button

--- grid.js
+++ grid.js
@@ -1,20 +1,53 @@
 //this project is to practice DOM manipulation
-let widthSquares = 16, heightSquares = 16;
 let containerDiv = document.getElementById('container');
+let clearGridButton = document.querySelector('button');
+createGrid(16,16);
 
+clearGridButton.addEventListener('click', ()=> {clearGrid()});
 
-for(let i = 0; i < widthSquares; i++)
+function createGrid(widthSquares, heightSquares)
 {
-    for(let j = 0; j < heightSquares; j++)
+    let heightBoxPercent = 92.5/heightSquares;
+    let widthBoxPercent = 100/widthSquares;
+    for(let i = 0; i < widthSquares; i++)
     {
-        createNewDiv();
+        for(let j = 0; j < heightSquares; j++)
+        {
+            createNewDiv(widthBoxPercent, heightBoxPercent);
+        }
     }
 }
 
-
-function createNewDiv()
+function createNewDiv(widthBoxPercent,heightBoxPercent)
 {
     const newDiv = document.createElement('div');
     newDiv.classList.add('boxes');
+    newDiv.style.cssText = 'height:'+heightBoxPercent+'% !important;'+'width:'+widthBoxPercent+'% !important;'
+    newDiv.addEventListener('mouseenter', () => {newDiv.classList.add('change-color')});
     containerDiv.appendChild(newDiv);
 }
+
+function clearGrid()
+{
+    let newWidthSquares = prompt("Plz gib me new num of squares 4 width. Don't exceed 100 please.");
+    while(newWidthSquares > 100)
+    {
+        newWidthSquares = prompt("Please enter a valid input...");
+    }
+    let newHeightSquares = prompt("Plz gib me new num of squares 4 height. Don't exceed 100 please.");
+    while(newHeightSquares > 100)
+    {
+        newHeightSquares = prompt("Please enter a valid input...");
+    }
+    let allBoxes = document.querySelectorAll('.boxes');
+    
+    for(let i =0; i < allBoxes.length; i++)
+    {
+        containerDiv.removeChild(containerDiv.lastChild);
+    }
+
+    createGrid(newWidthSquares, newHeightSquares);
+
+}
+
+
